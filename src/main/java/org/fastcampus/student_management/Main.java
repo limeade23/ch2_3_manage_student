f4436e6429294ba25dad3b40b6c08f6c83f7648d
limeade23 <eriol72@limeade.me>
2025-02-17 19:50:27 +0900

    finish

--- src/main/java/org/fastcampus/student_management/Main.java
+++ src/main/java/org/fastcampus/student_management/Main.java
@@ -1,7 +1,9 @@
 package org.fastcampus.student_management;
 
 import org.fastcampus.student_management.application.course.CourseService;
+import org.fastcampus.student_management.application.course.dto.CourseInfoDto;
 import org.fastcampus.student_management.application.student.StudentService;
+import org.fastcampus.student_management.application.student.dto.StudentInfoDto;
 import org.fastcampus.student_management.repo.CourseRepository;
 import org.fastcampus.student_management.repo.StudentRepository;
 import org.fastcampus.student_management.ui.course.CourseController;
@@ -24,6 +26,17 @@ public class Main {
 
     CourseController courseController = new CourseController(coursePresenter, courseService, studentPresenter);
     StudentController studentController = new StudentController(studentPresenter, studentService);
+
+    // 기본 dummy값
+    StudentInfoDto s1 = new StudentInfoDto("홍길동", 20, "서울시 강남구");
+    StudentInfoDto s2 = new StudentInfoDto("김길동", 21, "서울시 노원구");
+    StudentInfoDto s3 = new StudentInfoDto("강길동", 22, "서울시 용산구");
+    studentService.saveStudent(s1); studentService.saveStudent(s2); studentService.saveStudent(s3);
+
+    CourseInfoDto c1 = new CourseInfoDto("바이올린", 1500, "MONDAY", "홍길동", 1717299008L);
+    CourseInfoDto c2 = new CourseInfoDto("첼로", 1200, "SUNDAY", "강길동", 1717299008L);
+    CourseInfoDto c3 = new CourseInfoDto("피아노", 1100, "FRIDAY", "김길동", 1717299008L);
+    courseService.registerCourse(c1); courseService.registerCourse(c2); courseService.registerCourse(c3);
 
     studentPresenter.showMenu();
     UserInputType userInputType = studentController.getUserInput();
